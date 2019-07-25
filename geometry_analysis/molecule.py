d181b512acba7ccd320b76139ed07904e6e28e73
Sabiha Rustam <sabiha3@uw.edu>
2019-07-25 10:19:34 -0500

    used yapf to make it pep8 style

--- geometry_analysis/molecule.py
+++ geometry_analysis/molecule.py
@@ -4,7 +4,6 @@ TA python package for the MolSSI Software Summer School.
 
 Handles the primary functions
 """
-
 """
 molecule.py
 A python package for the MolSSI Software Summer School.
@@ -13,6 +12,7 @@ Contains a molecule class
 """
 
 import numpy as np
+
 
 def calculate_distance(rA, rB):
     """Calculate the distance between points A and B.
@@ -34,9 +34,10 @@ def calculate_distance(rA, rB):
     >>> calculate_distance(np.array([0, 0, 0], [0, 0.1, 0]))
     0.1
     """
-    dist_vec = (rA-rB)
+    dist_vec = (rA - rB)
     distance = np.linalg.norm(dist_vec)
     return distance
+
 
 def calculate_angle(rA, rB, rC, degrees=False):
     """Calculate angle between points A, B, and C
@@ -57,13 +58,14 @@ def calculate_angle(rA, rB, rC, degrees=False):
     """
     AB = rB - rA
     BC = rB - rC
-    
-    theta = np.arccos(np.dot(AB, BC) / (np.linalg.norm(AB)*np.linalg.norm(BC)))
-    
-    if degrees: 
+
+    theta = np.arccos(np.dot(AB, BC) / (np.linalg.norm(AB) * np.linalg.norm(BC)))
+
+    if degrees:
         return np.degrees(theta)
     else:
         return theta
+
 
 class Molecule:
     def __init__(self, name, symbols, coordinates):
@@ -71,25 +73,24 @@ class Molecule:
             self.name = name
         else:
             raise TypeError("Name is not a string.")
-        
+
         self.symbols = symbols
         self._coordinates = coordinates
         self.bonds = self.build_bond_list()
-    
+
     @property
     def num_atoms(self):
         return len(self.coordinates)
-    
+
     @property
     def coordinates(self):
         return self._coordinates
-    
+
     @coordinates.setter
     def coordinates(self, new_coordinates):
         self._coordinates = new_coordinates
         self.bonds = self.build_bond_list()
 
-    
     def build_bond_list(self, max_bond=2.93, min_bond=0):
         """
         Build a list of bonds based on a distance criteria.
@@ -107,18 +108,18 @@ class Molecule:
         bond_list : list
             List of bonded atoms. Returned as list of tuples where the values are the atom indices.
         """
-        
+
         bonds = {}
-        
+
         for atom1 in range(self.num_atoms):
             for atom2 in range(atom1, self.num_atoms):
                 distance = calculate_distance(self.coordinates[atom1], self.coordinates[atom2])
-                
+
                 if distance > min_bond and distance < max_bond:
                     bonds[(atom1, atom2)] = distance
-        
+
         return bonds
-    
+
 
 if __name__ == "__main__":
     # Do something if this file is invoked on its own
